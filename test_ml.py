6fd7ad9551a70348e22e47b2563a821686c76fd5
Sadie Martello <smarte21@wgu.edu>
2025-10-15 00:10:49 -0400

    add ML pipeline, unit tests, and model slice evaluation. Add unit test screenshot.

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,92 @@
 import pytest
-# TODO: add necessary import
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml.model import train_model, inference, compute_model_metrics
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+# Load small subset of dataset for testing
+data_path = "./data/census.csv"
+data = pd.read_csv(data_path)
+
+# random sample for testing
+test_sample = data.sample(n=50, random_state=42)
+
+cat_features = [
+    "workclass", "education", "marital-status", "occupation", 
+    "relationship", "race", "sex", "native-country"
+]
+
+# split sample into train/test
+train_df, test_df = train_test_split(test_sample, test_size=0.2, random_state=42)
+
+# Test #1: ML functions return expected type
+def test_process_train_inference_types():
     """
-    # add description for the first test
+    Test that processed data, labels, and predictions are numpy arrays or expected shape.
     """
-    # Your code here
-    pass
+    X_train, y_train, encoder, lb = process_data(
+        train_df,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+    model = train_model(X_train, y_train)
+    X_test, y_test, _ ,_ = process_data(
+        test_df,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb
+    )
+    preds = inference(model, X_test)
+
+    assert isinstance(X_test, np.ndarray)
+    assert isinstance(y_test, np.ndarray)
+    assert isinstance(preds, np.ndarray)
+    assert preds.shape == y_test.shape
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+# Test #2: ML Model uses expected algorithm
+def test_model_algorithm():
     """
-    # add description for the second test
+    Test that the trained model is a RandomForestClassifier.
     """
-    # Your code here
-    pass
+    X_train, y_train, _ ,_ = process_data(
+        train_df,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier)
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+# Test #3: compute_model_metrics returns expected value type
+def test_compute_metrics_types():
     """
-    # add description for the third test
+    Test that compute_model_metrics returns three floats: precision, recall, f1.
     """
-    # Your code here
-    pass
+    X_train, y_train, encoder, lb = process_data(
+        train_df,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+    model = train_model(X_train, y_train)
+    X_test, y_test, _ ,_ = process_data(
+        test_df,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb
+    )
+    preds = inference(model, X_test)
+    p, r, f1 = compute_model_metrics(y_test, preds)
+
+    assert isinstance(p, float)
+    assert isinstance(r, float)
+    assert isinstance(f1, float)
